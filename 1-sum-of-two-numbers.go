4699f8cef7d80cc8bce2341520212918b9575052
BlitheWells <1589439+BlitheWells@users.noreply.github.com>
2020-04-29 22:47:14 +0800

    Create 1-sum-of-two-numbers.go

---
+++ 1-sum-of-two-numbers.go
@@ -0,0 +1,16 @@
+```go
+// force way, two loop
+func twoSum(nums []int, target int) []int {
+    var location []int
+    for i := 0; i < len(nums); i ++ {
+        for j := i + 1; j < len(nums); j ++ {
+            x , y := nums[i], nums[j]
+            if x + y == target {
+                location = []int{i, j}
+                break
+            }
+        } 
+    }
+    return location
+}
+```
